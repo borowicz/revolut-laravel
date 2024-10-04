ed2b092c9a368f4519b0dd46cf476fd00ab5d5de
borowicz <borowicz@users.noreply.github.com>
2024-10-04 13:48:58 +0200

    update models

--- app/Models/Revolut/StockRobo/StockRoboTransaction.php
+++ app/Models/Revolut/StockRobo/StockRoboTransaction.php
@@ -2,16 +2,26 @@
 
 namespace App\Models\Revolut\StockRobo;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Revolut\AbstractRevolutModel;
 
+/**
+ *- -***
+ */
 class StockRoboTransaction extends AbstractRevolutModel
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-
+        'hash',
+        'date',
+        'ticker',
+        'type',
+        'quantity',
+        'price',
+        'total',
+        'currency',
+        'rate',
     ];
 }
