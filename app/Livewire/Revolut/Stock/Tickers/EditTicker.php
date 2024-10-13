d37dd85acd0f74c50d24c6066d742fa67b9d5b66
borowicz <borowicz@users.noreply.github.com>
2024-10-13 10:46:31 +0200

    tickers

--- app/Livewire/Revolut/Stock/Tickers/EditTicker.php
+++ app/Livewire/Revolut/Stock/Tickers/EditTicker.php
@@ -14,14 +14,20 @@ class EditTicker extends Component
     public $stockMarkets;
 
     public $id;
-    public $disabled = '';
+    public $disabled;
     public $hash = '';
     public $ticker = '';
     public $url = '';
     public $notes = '';
     public $stock_markets_id = '';
+    public $entryInfo = [
+        'hash' => '',
+        'created_at' => '',
+        'updated_at' => '',
+        'deleted_at' => '',
+    ];
 
-    public function mount(StockTicker $item)
+    public function mount()
     {
         $this->item = StockTicker::find($this->id);
 
@@ -31,6 +37,18 @@ class EditTicker extends Component
         $this->url = $this->item->url ?? '';
         $this->notes = $this->item->notes ?? '';
         $this->stock_markets_id = $this->item->stock_markets_id ?? '';
+        $this->entryInfo = [
+            'hash'       => $this->item->hash ?? '',
+            'created_at' => $this->item->created_at ?? '',
+            'deleted_at' => $this->item->deleted_at ?? '',
+            'updated_at' => $this->item->updated_at ?? '',
+        ];
+    }
+
+    public function toggleDisabled(int $status)
+    {
+        $this->disabled = !$this->disabled;
+        $this->item->update(['disabled' => $this->disabled]);
     }
 
     public function rules()
@@ -51,12 +69,12 @@ class EditTicker extends Component
         $this->validate();
 
         $this->item->update([
-                                'disabled' => $this->disabled,
-                                'ticker'   => $this->ticker,
-                                'url'      => $this->url,
-                                'notes'    => $this->notes,
-                                'stock_markets_id' => $this->stock_markets_id,
-                            ]);
+            'disabled' => $this->disabled,
+//            'ticker'   => $this->ticker,
+            'url'      => $this->url,
+            'notes'    => $this->notes,
+            'stock_markets_id' => $this->stock_markets_id,
+        ]);
 
         return redirect()->to(route('stock.tickers'));
     }
--- resources/views/livewire/revolut/stock/tickers-form.blade.php
+++ resources/views/livewire/revolut/stock/tickers-form.blade.php
@@ -4,103 +4,114 @@
     </h2>
 </x-slot>
 
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                <header>
-                    <h2 class="text-lg font-medium text-gray-900">
-                        {{ __($buttonAction) }}
-                    </h2>
-                    <div class="mt-1 text-sm text-gray-600">
-                        @include('livewire.partials.sub-menu-button', ['menuRoute' => 'stock.tickers', 'menuText' => 'back'])
-                    </div>
-                </header>
+<div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
 
-                <form wire:submit.prevent="save" class="mt-6 space-y-6">
-                    @csrf
-                    <div>
-                        <x-input-label for="disabled" :value="__('Disabled')" />
-                        <label for="disabled" class="flex items-center cursor-pointer">
-                            <div class="relative">
-                                <input type="checkbox" id="disabled" wire:model="disabled" class="sr-only">
-                                <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                                <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                            </div>
-                            <div class="ml-3 text-gray-700 font-medium">
+        <form wire:submit.prevent="save" class="mt-6 space-y-6">
+            @csrf
+
+            <header>
+                <div class="float-right mt-1 text-sm text-gray-600">
+                    @include('livewire.partials.sub-menu-button', ['menuRoute' => 'stock.tickers', 'menuText' => 'back'])
+                </div>
+                <h2 class="text-lg font-medium text-gray-900">
+                    {{ __($buttonAction) }}
+                </h2>
+            </header>
+
+            <div class="grid grid-cols-3 gap-4">
+
+                <!-- Pole 'Hash' -->
+                <div>
+                    <x-input-label for="hash" :value="__('Hash')"/>
+                    <x-text-input wire:model="hash"
+                                  id="hash"
+                                  name="hash"
+                                  type="text"
+                                  readonly="readonly"
+                                  class="mt-1 block w-full" required autocomplete="hash"/>
+                    <x-input-error class="mt-2" :messages="$errors->get('hash')"/>
+                </div>
+
+                <div>
+                    <label class="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" value="1"
+                               wire:model="disabled"
+                               id="disabled"
+                               class="sr-only peer"
+                               {{ !$disabled ?: 'checked' }}
+                               wire:click="toggleDisabled('{{ $disabled }}')"
+                        >
+                        <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-gray-300 dark:peer-focus:ring-gray-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+                        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            @if($disabled)
+                                {{ __('Enabled') }}
+                            @else
                                 {{ __('Disabled') }}
-                            </div>
-                        </label>
-                        <x-input-error class="mt-2" :messages="$errors->get('disabled')" />
-                    </div>
+                            @endif
+                        </span>
+                    </label>
+                </div>
 
-                    <div>
-                        <x-input-label for="hash" :value="__('Hash')" />
-                        <x-text-input wire:model="hash"
-                                      id="hash"
-                                      name="hash"
-                                      type="text"
-                                      class="mt-1 block w-full" required autocomplete="hash" />
-                        <x-input-error class="mt-2" :messages="$errors->get('hash')" />
-                    </div>
+                <!-- Checkbox 'Disabled' -->
+                <div class="text-sm">
+                    {{ __('created') }} {{ $entryInfo['created_at'] ?? '' }}<br/>
+                    {{ __('updated') }} {{ $entryInfo['updated_at'] ?? '' }}<br/>
+                    {{ __('hash') }} {{ $entryInfo['hash'] ?? '' }}<br/>
+                </div>
 
-                    <div>
-                        <x-input-label for="ticker" :value="__('Ticker')" />
-                        <x-text-input wire:model="ticker"
-                                      id="ticker"
-                                      name="ticker"
-                                      type="text"
-                                      class="mt-1 block w-full" required autocomplete="ticker" />
-                        <x-input-error class="mt-2" :messages="$errors->get('ticker')" />
-                    </div>
+                <!-- Pole 'Ticker' -->
+                <div>
+                    <x-input-label for="ticker" :value="__('Ticker')"/>
+                    <x-text-input wire:model="ticker"
+                                  id="ticker"
+                                  name="ticker"
+                                  type="text"
+                                  class="mt-1 block w-full" required autocomplete="ticker"/>
+                    <x-input-error class="mt-2" :messages="$errors->get('ticker')"/>
+                </div>
 
-                    <div>
-                        <x-input-label for="url" :value="__('URL')" />
-                        <x-text-input wire:model="url"
-                                      id="url"
-                                      name="url"
-                                      type="text"
-                                      class="mt-1 block w-full" required autocomplete="url" />
-                        <x-input-error class="mt-2" :messages="$errors->get('url')" />
-                    </div>
+                <!-- Select 'Stock Market' -->
+                <div class="col-span-2">
+                    <x-input-label for="stock_markets_id" :value="__('Stock Market')"/>
+                    <select wire:model="stock_markets_id" id="stock_markets_id" name="stock_markets_id"
+                            class="mt-1 w-full">
+                        <option value="">{{ __('Select Stock Market') }}</option>
+                        @foreach($stockMarkets as $stockMarket)
+                            <option
+                                value="{{ $stockMarket->id }}" {{ $stockMarket->id == $stock_markets_id ? 'selected' : '' }}>
+                                {{ $stockMarket->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <x-input-error class="mt-2" :messages="$errors->get('stock_markets_id')"/>
+                </div>
 
-                    <div>
-                        <x-input-label for="notes" :value="__('Notes')" />
-                        <x-text-input wire:model="notes"
-                                      id="notes"
-                                      name="notes"
-                                      type="text"
-                                      class="mt-1 block w-full" required autocomplete="notes" />
-                        <x-input-error class="mt-2" :messages="$errors->get('notes')" />
-                    </div>
+                <!-- Pole 'URL' -->
+                <div class="col-span-3">
+                    <x-input-label for="url" :value="__('URL')"/>
+                    <x-text-input wire:model="url"
+                                  id="url"
+                                  name="url"
+                                  type="text"
+                                  class="mt-1 block w-full" required autocomplete="url"/>
+                    <x-input-error class="mt-2" :messages="$errors->get('url')"/>
+                </div>
 
-                    <div>
-                        <x-input-label for="stock_markets_id" :value="__('Stock Market')" />
-                        <select wire:model="stock_markets_id" id="stock_markets_id" name="stock_markets_id" class="mt-1 block w-full">
-                            <option value="">{{ __('Select Stock Market') }}</option>
-                            @foreach($stockMarkets as $stockMarket)
-                                <option value="{{ $stockMarket->id }}" {{ $stockMarket->id == $stock_markets_id ? 'selected' : '' }}>
-                                    {{ $stockMarket->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                        <x-input-error class="mt-2" :messages="$errors->get('stock_markets_id')" />
-                    </div>
+                <!-- Pole 'Notes' -->
+                <div class="col-span-3">
+                    <x-input-label for="notes" :value="__('Notes')"/>
+                    <x-text-input wire:model="notes"
+                                  id="notes"
+                                  name="notes"
+                                  type="text"
+                                  class="mt-1 block w-full" required autocomplete="notes"/>
+                    <x-input-error class="mt-2" :messages="$errors->get('notes')"/>
+                </div>
 
-                    <div class="flex items-center gap-4">
-                        <x-primary-button>{{ __($buttonAction) }}</x-primary-button>
-                        &nbsp;
-                        <button type="button"
-                                class="inline-flex items-center px-4 py-2 rounded-md
-                                       font-semibold text-xs text-white uppercase
-                                       active:bg-gray-500 active:text-gray-700
-                                       hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100
-                                       bg-gray-500
-                                       transition ease-in-out duration-150"
-                                wire:click="cancel">{{ __('Cancel') }}</button>
-                    </div>
-                </form>
             </div>
-        </div>
+
+        </form>
+
     </div>
 </div>
